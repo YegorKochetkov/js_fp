44d74c59aad9d35830a7351c0a38b9bc6c1e41cb
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-09-28 16:24:12 +0300

    feat: show list of text from input form;
    move several functions to separate file;
    update jsdoc

---
+++ public/helpers.js
@@ -0,0 +1,108 @@
+/**
+ * Creates a new HTML element with the specified tag name.
+ *
+ * @param {string} tag - The tag name of the HTML element to be created.
+ * @returns {HTMLElement} A new HTML element with the specified tag name.
+ */
+const createElement = (tag) => document.createElement(tag);
+
+/**
+ * Creates a text node with the specified content.
+ *
+ * @param {string} content - The content to be set for the text node.
+ * @returns {TextNode} The text node with the specified content.
+ */
+const createText = (content) => document.createTextNode(content);
+
+/**
+ * Retrieves the HTML element with the specified id.
+ *
+ * @param {string} id - The id of the HTML element to be retrieved.
+ * @returns {HTMLElement} - The HTML element with the specified id.
+ */
+const getElementById = (id) => document.getElementById(id);
+
+/**
+ * Retrieves the value of an input element with the id "input".
+ *
+ * @returns {string} The text entered by the user in the input element with the id "input".
+ */
+const getTextFromInput = () => getElementById("input").value.trim();
+
+/**
+ * Sets the value of an input element with the id "input".
+ *
+ * @param {string} value - The value to be set for the input element.
+ * @returns {void} No return value.
+ */
+const setTextToInput = (value) => (getElementById("input").value = value);
+
+/**
+ * A curried version of the function that adds a specified class to an element's class list.
+ *
+ * @param {string} className - The name of the class to be added.
+ * @param {HTMLElement} element - The element to which the class will be added.
+ * @returns {HTMLElement} The modified element with the specified class added to its class list.
+ */
+const addClass = R.curry(function (className, element) {
+  element.classList.add(className);
+
+  return element;
+});
+
+/**
+ * A curried version of the function that appends a given node to a given element.
+ *
+ * @param {Node} node - The node to be appended to the element.
+ * @param {HTMLElement} element - The element to which the node will be appended.
+ * @returns {HTMLElement} The element with the node appended to it.
+ */
+const appendChild = R.curry(function (node, element) {
+  element.appendChild(node);
+
+  return element;
+});
+
+/**
+ * A curried version of the function that sets a specified attribute with a given value
+ * on a given HTML element.
+ *
+ * @param {string} attributeName - The name of the attribute to be set.
+ * @param {string} attributeValue - The value to be set for the attribute.
+ * @param {HTMLElement} element - The HTML element on which the attribute will be set.
+ * @returns {HTMLElement} The modified element with the specified attribute set.
+ */
+const setAttribute = R.curry(function (attributeName, attributeValue, element) {
+  element.setAttribute(attributeName, attributeValue);
+
+  return element;
+});
+
+/**
+ * A curried version of the function that creates an event listener for
+ * a specified event type on a given HTML element, and associates it with a callback function.
+ *
+ * @param {string} eventType - The type of event to listen for.
+ * @param {HTMLElement} element - The HTML element to attach the event listener to.
+ * @param {function} fn - The callback function to be executed when the event is triggered.
+ * @returns {function} A function that removes the event listener when called.
+ */
+const on = R.curry((eventType, element, fn) => {
+  element.addEventListener(eventType, fn);
+
+  return function () {
+    element.removeEventListener(eventType, fn);
+  };
+});
+
+/**
+ * A curried version of the function that clears the content of the given element.
+ *
+ * @param {Element} element - The element whose content will be cleared.
+ * @return {Element} The modified element with cleared content.
+ */
+const clearElement = R.curry((element) => {
+  element.innerHTML = "";
+
+  return element;
+});
--- public/index.js
+++ public/index.js
@@ -1,112 +1,65 @@
-const _R = R; // eslint-disable-line
-
-/**
- * Creates a new HTML element with the specified tag name.
- *
- * @param {string} tag - The tag name of the HTML element to be created.
- * @returns {HTMLElement} A new HTML element with the specified tag name.
- */
-const createElement = (tag) => document.createElement(tag);
-
-/**
- * Creates a text node with the specified content.
- *
- * @param {string} content - The content to be set for the text node.
- * @returns {TextNode} The text node with the specified content.
- */
-const createText = (content) => document.createTextNode(content);
-
-/**
- * A curried version of the function that adds a specified class to
- * an element's class list.
- */
-const addClass = _R.curry(
-  /**
-   * A function that adds a specified class to an element's class list.
-   *
-   * @param {string} className - The name of the class to be added.
-   * @param {HTMLElement} element - The element to which the class will be added.
-   * @returns {HTMLElement} The modified element with the specified class added to its class list.
-   */
-  function (className, element) {
-    element.classList.add(className);
-
-    return element;
-  }
-);
-
-/**
- * A curried version of the function that appends a given node to a given element.
- */
-const appendChild = _R.curry(
-  /**
-   * A function that appends a given node to a given element.
-   *
-   * @param {Node} node - The node to be appended to the element.
-   * @param {HTMLElement} element - The element to which the node will be appended.
-   * @returns {HTMLElement} The element with the node appended to it.
-   */
-  function (node, element) {
-    element.appendChild(node);
-
-    return element;
-  }
-);
-
-/**
- * A curried version of the function that sets a specified attribute
- * with a given value on a given HTML element.
- */
-const setAttribute = _R.curry(
-  /**
-   * A function that sets a specified attribute with a given value
-   * on a given HTML element.
-   *
-   * @param {string} attributeName - The name of the attribute to be set.
-   * @param {string} attributeValue - The value to be set for the attribute.
-   * @param {HTMLElement} element - The HTML element on which the attribute will be set.
-   * @returns {HTMLElement} The modified element with the specified attribute set.
-   */
-  function (attributeName, attributeValue, element) {
-    element.setAttribute(attributeName, attributeValue);
-
-    return element;
-  }
-);
-
 /**
  * A curried version of the function that creates a new DOM element with
  * specified attributes and classes, then appends it to another element.
+ * Uses the `R.compose()` function from the Ramda library to apply a series of
+ * transformations to the element in a functional and composable way.
+ *
+ * @param {string} message - The text content of the new element.
+ * @param {string} data_message - The value of the `data-message` attribute for the new element.
+ * @returns {HTMLElement} A new DOM element with the specified attributes and content.
  */
-const message = _R.curry(
-  /**
-   * Creates a new DOM element with specified attributes and classes,
-   * then appends it to another element.
-   * Uses the `R.compose()` function from the Ramda library to apply a series of
-   * transformations to the element in a functional and composable way.
-   *
-   * @param {string} message - The text content of the new element.
-   * @param {string} data_message - The value of the `data-message` attribute for the new element.
-   * @returns {HTMLElement} A new DOM element with the specified attributes and content.
-   */
-  function (message, data_message) {
-    return _R.compose(
-      appendChild(createText(message)),
-      setAttribute("data-message", data_message),
-      addClass("text-bg-primary"),
-      addClass("p-4")
-    )(createElement("div"));
-  }
-);
+const message = R.curry(function (message, data_message) {
+  return R.compose(
+    appendChild(createText(message)),
+    setAttribute("data-message", data_message),
+    addClass("text-bg-primary"),
+    addClass("p-3"),
+    addClass("mb-3"),
+    addClass("rounded")
+  )(createElement("div"));
+});
 
 /**
- * Sets a specific data-message attribute for the div element.
+ * Generates a view based on the given state.
  *
- * @param {string} data_message - The value of the `data-message` attribute for the div element.
- * @returns {HTMLElement} Return the div element with `data-message` attribute.
+ * @param {Array} state - The state to generate the view from.
+ * @return {HTMLElement} - The generated view element.
  */
-const answer = message(
-  "The Answer to the Ultimate Question of Life, The Universe, and Everything"
-);
+const view = (state) => {
+  const element = createElement("div");
 
-document.body.appendChild(answer("42"));
+  const appendFunctions = state.map((content, index) =>
+    appendChild(message(content, index))
+  );
+
+  return state.length > 0
+    ? R.pipe(...appendFunctions)(createElement("div"))
+    : element;
+};
+
+function app(state, outputElement, dispatch) {
+  R.compose(appendChild(view(state)), clearElement())(outputElement);
+
+  const stop = dispatch((event) => {
+    event.preventDefault();
+    stop();
+
+    const newText = getTextFromInput();
+    const newState = newText.length > 0 ? [...state, newText] : state;
+
+    setTextToInput("");
+
+    app(newState, outputElement, dispatch);
+  });
+}
+
+/**
+ * Creates an event listener for a button click event.
+ *
+ * @param {string} eventType - The type of event to listen for (e.g. "click").
+ * @param {Element} targetElement - The target element to attach the event listener to.
+ * @returns {EventListener} - The event listener for the button click event.
+ */
+const buttonClick = on("click", getElementById("button"));
+
+app(Object.freeze([]), getElementById("messages"), buttonClick);
